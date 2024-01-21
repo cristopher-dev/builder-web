b172be3c003805fd583ec18e2a32352b89b294c7
cristopher-dev <cristopher-dev>
2024-01-20 20:15:07 -0500

    feat: plugin blocks-basic

--- src/plugin/plugin.tsx
+++ src/plugin/plugin.tsx
@@ -3,15 +3,4 @@ export const plugin = [
     id: 'gjs-blocks-basic',
     src: 'https://unpkg.com/grapesjs-blocks-basic',
   },
-  {
-    id: 'grapesjs-symbols',
-    src: 'https://unpkg.com/@silexlabs/grapesjs-symbols',
-    options: {
-      '@silexlabs/grapesjs-symbols': {
-        appendTo: '.gjs-pn-views-container',
-      },
-    },
-  },
-
-  
 ];
